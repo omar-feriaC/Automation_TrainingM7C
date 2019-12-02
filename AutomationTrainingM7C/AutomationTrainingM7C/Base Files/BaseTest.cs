fc30b86c02b9ffc9cbb434eda6884e736c2c4e23
Josetziu4 <47648101+Josetziu4@users.noreply.github.com>
2019-12-02 16:11:46 -0600

    Session 8, Nov 27, 2019

---
+++ AutomationTrainingM7C/AutomationTrainingM7C/Base Files/BaseTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Configuration;
+
+namespace AutomationTrainingM7C.Base_Files
+{
+    class BaseTest
+    {
+        public IWebDriver driver = new ChromeDriver();
+
+        public string url = ConfigurationManager.AppSettings.Get("url");
+
+        
+    }
+}
