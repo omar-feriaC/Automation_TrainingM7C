140dbaaf03dc8f7b70c96ecd12ad2c7df5e9eb49
mitzioroz <47648163+mitzioroz@users.noreply.github.com>
2019-12-09 17:25:31 -0600

    1st Block

---
+++ AutomationTrainingM7C/AutomationTrainingM7C/Mitzi_Orozco_M7_Exercise/2D_Shape.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace AutomationTrainingM7C.Mitzi_Orozco_M7_Excercise
+{
+    class _2D_Shape : Ishape
+    {
+        Double Area(Double )
+    }
+
+}
---
+++ AutomationTrainingM7C/AutomationTrainingM7C/Mitzi_Orozco_M7_Exercise/IShape.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationTrainingM7C.Mitzi_Orozco_M7_Exercise
+{
+    interface IShape
+    {
+        strName();
+        DisplayInfo();
+    }
+}
