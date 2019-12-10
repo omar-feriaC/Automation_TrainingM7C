f14e4e9903fc8ee58b909b41b2185777551da22f
Saúl García <saul.garcia@4thsource.com>
2019-12-10 17:19:29 -0600

    Saul Garcia - Parte 2

---
+++ AutomationTrainingM7C/AutomationTrainingM7C/Base Files/Cylinder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationTrainingM7C.Base_Files
+{
+    class Cylinder : 3D_Shape
+    {
+    
+        public void Area()
+    { 
+
+            double pi=3.1416;
+            double radio=5;
+
+            ACylinder = pi * (5 * 5);
+            Console.WriteLine("Area = " +ACylinder)
+    }
+
+        public void Perimeter()
+        {
+            double radio = 3;
+            double pi = 3.1416;
+
+            PCylinder = radio * (2 * pi);
+            Console.WriteLine("Perimeter = " + PCylinder)
+        }
+
+        public void Volume()
+        {
+            double pi = 3.1416;
+            double radio = 5;
+            double high = 10;
+
+            VCylinder = pi * (5 * 5) * high;
+            Console.WriteLine("Volume = " + VCylinder)
+        }
+
+}
+}
---
+++ AutomationTrainingM7C/AutomationTrainingM7C/Base Files/Hexagon.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationTrainingM7C.Base_Files
+{
+    class Hexagon : 2D_Shape
+    {
+        public Area()
+        {
+
+            double perihexa = 5;
+            double apo = 3;
+
+            AHexagon = (perihexa * apo) / 2;
+            Console.WriteLine("Area = " + AHexagon)
+    }
+
+        public void Perimeter()
+        {
+            double length = 1;
+            
+            PHexagon = length * 6;
+            Console.WriteLine("Perimeter = " + PHexagon)
+        }
+    }
+}
