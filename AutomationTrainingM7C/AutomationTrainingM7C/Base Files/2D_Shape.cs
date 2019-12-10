8693643e85c404f43f4a097043700a0502633fe7
Saúl García <saul.garcia@4thsource.com>
2019-12-10 08:52:17 -0600

    Saul Garcia - Parte 1

---
+++ AutomationTrainingM7C/AutomationTrainingM7C/Base Files/2D_Shape.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationTrainingM7C.Base_Files
+{
+    class 2D_Shape
+    {
+        string name = undefined;
+        double Area = 0;
+        double Perimeter = 0;
+        
+        public interface IShape
+        {
+        double Area { };
+        double Perimeter { };
+        string Name { };
+        void DisplayInfo();
+        }
+
+    }
+
+    class 3D_Shape
+    {
+    string Name = undefined;
+    double Base = 0;
+    double Area = 0;
+    double Perimeter = 0;
+    
+
+    public interface IShape
+    {
+    double Volume { };
+    void DisplayInfo();
+    }
+    }
+}
